bf9f4e0a3b566fa99603f412cf27032f9fcdec8f
khoa <khoale22122k5@gmail.com>
2026-03-17 10:30:42 +0700

    final version

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.demo.entity.User;
 import com.example.demo.response.ApiResponse;
 import com.example.demo.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,50 +14,45 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
-
     public UserController(UserService userService){
         this.userService = userService;
     }
 
     @PostMapping
-    public ApiResponse<User> createUser(@Valid @RequestBody UserRequest request){
-
+    public ApiResponse<User> creatUser(@Valid @RequestBody UserRequest request){
+        log.info("Bắt đầu tạo mới user có email là {}", request.getEmail());
         User user = userService.createUser(request);
-
+        log.info("Tạo User thành công! ID được cấp là: {}", user.getId());
         return new ApiResponse<>(0, "success", user);
+
     }
 
     @GetMapping
     public ApiResponse<List<User>> getAllUser(){
-
         List<User> users = userService.getUsers();
-
         return new ApiResponse<>(0, "success", users);
     }
 
     @GetMapping("/{id}")
     public ApiResponse<User> getUserById(@PathVariable Long id){
-
         User user = userService.getUserById(id);
-
         return new ApiResponse<>(0, "success", user);
     }
 
+
     @PutMapping("/{id}")
-    public ApiResponse<User> updateUserById(@PathVariable Long id,
-                                            @RequestBody UserRequest request){
-
+    public ApiResponse<User> updateUserById(@PathVariable Long id, @RequestBody UserRequest request){
         User user = userService.updateUserById(id, request);
-
         return new ApiResponse<>(0, "success", user);
     }
 
     @DeleteMapping("/{id}")
-    public ApiResponse<String> deleteUser(@PathVariable Long id){
-
+    public ApiResponse<String> deleteUserById(@PathVariable Long id){
         userService.deleteUserById(id);
-
         return new ApiResponse<>(0, "success", "User deleted");
     }
+
+
 }
--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -4,9 +4,12 @@ import com.example.demo.response.ApiResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ApiResponse<String> handleValidationException(MethodArgumentNotValidException ex){
@@ -14,12 +17,13 @@ public class GlobalExceptionHandler {
         String message = ex.getBindingResult()
                 .getFieldError()
                 .getDefaultMessage();
-
+        log.warn("Dữ liệu đầu vào không hợp lệ");
         return new ApiResponse<>(1, message, null);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ApiResponse<String> handleUserNotFound(UserNotFoundException ex){
+        log.error("Lỗi nghiệp vụ: {}", ex.getMessage());
         return new ApiResponse<>(1, ex.getMessage(), null);
     }
 
